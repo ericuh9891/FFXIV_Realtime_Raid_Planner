04286effee10b38062edf943863171352cba0258
Eric Y U Huang <ericuh9891@hotmail.com>
2022-10-27 22:43:35 -0400

    Added bounds for Draggable components to not leave the Arena component's area

--- client/src/components/Arena/Arena.js
+++ client/src/components/Arena/Arena.js
@@ -1,33 +1,61 @@
 import React from 'react';
 import './Arena.css';
+import Draggable from 'react-draggable';
 
 
 function Arena (props) {
-
   const [icons, setIcons] = React.useState([]);
+  const arenaRef = React.useRef();
 
   function onDragOverHandler(event) {
     event.preventDefault();
     event.dataTransfer.dropEffect = 'move';
   };
 
+  function onDragHandler(event, data) {
+    console.log(data);
+  }; 
+
   function onDropHandler(event) {
     event.preventDefault();
+    console.log(event);
+    console.log(arenaRef.current.getBoundingClientRect());
     const index = event.dataTransfer.getData('text/plain');
-
-    setIcons( (prevIcons) => {
-      const element = (
-        <div className='IconList-selection'
-          key={index}>
-          <img className='IconList-image' src={props.iconList[index]} id={index}></img>
-        </div>
-      );
-      return [...prevIcons, element];
-    })
+    if (index == null){
+      return;
+    }
+    else {
+      setIcons( (prevIcons) => {
+        const element = (
+          <Draggable
+            onDrag={onDragHandler}
+            defaultPosition={{x: 0, y: 0}}
+            bounds='parent'
+            >
+            <img 
+              className='Arena-Icon' 
+              src={props.iconList[index]} 
+              draggable='false' 
+              style={{
+                top: `${event.clientY-30}px`,
+                left: `${event.clientX - arenaRef.current.getBoundingClientRect().x -30}px`
+              }}
+            >
+            </img>
+          </Draggable>
+        );
+        return [...prevIcons, element];
+      });
+    }
   };
 
   return (
-    <div className='Arena' onDragOver={onDragOverHandler} onDrop={onDropHandler}>
+    <div 
+      className='Arena' 
+      ref={arenaRef} 
+      onDragOver={onDragOverHandler} 
+      onDrop={onDropHandler}
+    >
       {icons}
     </div>
   );
